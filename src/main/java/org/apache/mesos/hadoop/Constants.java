5e427c3ff44f42cfbd332c9bbfc488586706fa82
Benjamin Hindman <benjamin.hindman@gmail.com>
2013-07-28 20:57:02 -0700

    Passed the serialized configuration through TaskInfo.data.
    The serialized configuration is in a binary format and previously we
    passed this through an environment variable (which required Base64
    encoding). Instead, we pass the binary data without Base64 encoding
    through the TaskInfo.data field.

--- src/main/java/org/apache/mesos/hadoop/Constants.java
+++ src/main/java/org/apache/mesos/hadoop/Constants.java
@@ -1,9 +1,3 @@
 package org.apache.mesos.hadoop;
 
-/**
- * @author Florian Leibert (flo@leibert.de)
- */
-public class Constants {
-  public static final String HADOOP_MESOS_CONF_STRING = "HADOOP_MESOS_CONF_STRING";
-
-}
+public class Constants {}
